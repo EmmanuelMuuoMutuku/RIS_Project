f020e1a611af42d0bc689328b7da92a64eda38ae
Emmanuel Mutuku <83968326+EmmanuelMuuoMutuku@users.noreply.github.com>
2023-05-25 10:29:55 +0200

    left line update

--- object_detection/scripts/canny_func.py
+++ object_detection/scripts/canny_func.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 def region_of_interest(image):
     height, width = image.shape
     print(height, width)
-    # polygons = np.array([
-    #     [(0, height), (int(width/2), int(height/6)), (width, height)]
-    # ])
+   
     polygons = np.array([
         [(0, height), (0, int(height/2)),
          (width, int(height/2)), (width, height)]
@@ -21,10 +19,6 @@ def region_of_interest(image):
 
 
 def make_points(image, average):
-    # try:
-    #     slope, intercept = line_parameters
-    # except TypeError:
-    #     slope, intercept = 0,0
     slope, y_int = average
     y1 = image.shape[0]
     y2 = int(y1 * (3/5))
@@ -38,25 +32,31 @@ def average(image, lines):
     right = []
     if lines is not None:
         for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
             # print(x1, y1, x2, y22
-            # Finding parameters of a line
+            x1, y1, x2, y2 = line.reshape(4)
+            
+            # Find line parameters 
             parameters = np.polyfit((x1, x2), (y1, y2), 1)
-            # print(parameters)
+            
+            # print
             slope = parameters[0]
             y_int = parameters[1]
-            # Classify negative slope and positive slope
+            
+            # Classify -ve or +ve slope
             if slope < 0:
                 left.append((slope, y_int))
             else:
                 right.append((slope, y_int))
-        # Compute average of positive slope line and negative slope line
+                
+    # Compute average of slope lines
     right_avg = np.average(right, axis=0)
     left_avg = np.average(left, axis=0)
+    
     # if left_avg:
-    # put points in the image
+    # mark the image
     left_line = make_points(image, left_avg)
-    # print(left_line)
+    
+    # print
     # if right_avg:
     right_line = make_points(image, right_avg)
 
